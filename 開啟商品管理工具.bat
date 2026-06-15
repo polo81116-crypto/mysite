6ca1bf5b4baa7d3be6e16ab92a711fe7d0bc7a3d
polo81116-crypto <275896440+polo81116-crypto@users.noreply.github.com>
2026-06-15 14:46:17 +0800

    Fix admin tool launcher

--- 開啟商品管理工具.bat
+++ 開啟商品管理工具.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d "%~dp0"
+start "商品管理工具伺服器" /min powershell -NoProfile -ExecutionPolicy Bypass -Command "python -m http.server 5179"
+timeout /t 2 /nobreak >nul
 start "" "http://localhost:5179/商品管理工具.html"
-powershell -NoProfile -ExecutionPolicy Bypass -Command "python -m http.server 5179"
