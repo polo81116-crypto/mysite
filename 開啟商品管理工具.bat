d58040d071d6102ab55629e242e5da5a361db457
polo81116-crypto <275896440+polo81116-crypto@users.noreply.github.com>
2026-06-15 14:49:46 +0800

    Stabilize admin tool launcher

--- 開啟商品管理工具.bat
+++ 開啟商品管理工具.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-start "商品管理工具伺服器" /min powershell -NoProfile -ExecutionPolicy Bypass -Command "python -m http.server 5179"
+start "商品管理工具伺服器" /min powershell -NoProfile -ExecutionPolicy Bypass -Command "Set-Location '%~dp0'; python -m http.server 5179"
 timeout /t 2 /nobreak >nul
-start "" "http://localhost:5179/商品管理工具.html"
+start "" "http://127.0.0.1:5179/商品管理工具.html"
